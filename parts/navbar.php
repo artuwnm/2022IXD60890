a34ba4a584fa9a92a9f2341bb79361aa87927a99
김주연 <jennykim4019@gmail.com>
2022-10-22 21:15:28 -0700

    no message

--- parts/navbar.php
+++ parts/navbar.php
@@ -2,7 +2,7 @@
 	<header class="navbar">
 		<div class="container display-flex">
 			<div class="flex-none">
-				<h1>Store</h1>
+				<h1>Jenny's Online Store</h1>
 			</div>
 			<div class="flex-stretch"></div>
 			<nav class="nav nav-flex flex-none">
--- store_about.php
+++ store_about.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Product List</title>
+	<title>About</title>
 
 	<?php include "parts/meta.php"; ?>
 
@@ -10,6 +10,37 @@
 <body>
 	
 	<?php include "parts/navbar.php"; ?>
+	<script>
+		const makeTable = (classes='') => {
+			const head = ['', 'Office','Name','Email','Phone'];
+			const body = [
+				['522',"Hamilton cline",'hcline@academyart.edu','555-2424'],
+				['512',"Fred McHale",'fmchale@academyart.edu','555-2424'],
+				['522',"Michael Catanzaro",'mcdsfaksjf@academyart.edu','522-2424'],
+				['514',"Hyewon Koo",'jjmirae@hanmail.net','522-2424'],
+				['515',"Joohee Kim",'joohee@gmail.com','522-2323'],
+			];
+
+			//let ran= Math.floor(Math.random()*links.length);
+			document.write (`
+			<table class="${classes}">
+				<thead><tr>${head.reduce((r,o)=>r+`<th>${o}</th>`,'')}</tr></thead>
+				<tbody>${body.reduce((r,o,i)=>r+
+					`<tr>${
+						[i+1,...o].reduce((r,o)=>r+`<td>${o}</td>`,'')
+					}</tr>`,'')
+				}</tbody>
+			</table>
+			`);
+		};
+	</script>
+
+	<div class="card soft">
+			<h3>Contact</h3>
+
+			<script>makeTable("table lined vertical")</script>
+
+	</div> 
 
 </body>
 </html>

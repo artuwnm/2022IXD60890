4bb6c8cb0a4010e785d8b05a24164dd7266aed02
wanghua0309 <wanghua0309@gmail.com>
2022-10-22 21:19:32 -0700

    add new files

---
+++ wang.hua/parts/navbar.php
@@ -0,0 +1,17 @@
+
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Rainbow Toys</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+			   <ul>
+			      <li><a href="home.php">Home</a></li>
+			      <li><a href="product_list.php">Product</a></li>
+			      <li><a href="about.php">About</a></li>
+			      <li><a href="cart.php">Cart</a></li>
+			   </ul>
+			</nav>
+		</div>
+	</header>

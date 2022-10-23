430f9430ed0c35e303aa6cea147a7408f721de35
김주연 <jennykim4019@gmail.com>
2022-10-22 20:32:07 -0700

    nav update

--- parts/navbar.php
+++ parts/navbar.php
@@ -7,8 +7,8 @@
 			<div class="flex-stretch"></div>
 			<nav class="nav nav-flex flex-none">
 				<ul>
-					<li><a href="..notes/index.php">Home</a></li>
-					<li><a href="product_list.php">Products</a></li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
 					<li><a href="store_about.php">About</a></li>
 					<li><a href="product_cart.php">Cart</a></li>
 				</ul>	

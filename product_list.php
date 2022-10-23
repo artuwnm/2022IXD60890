8ec11e71c232bf45c56ae5d68e41279f0d4aa559
김주연 <jennykim4019@gmail.com>
2022-10-22 20:37:32 -0700

    list update

--- product_list.php
+++ product_list.php
@@ -16,10 +16,10 @@
 			<h2>Product List</h2>
 
 			<ul>
-				<li><a href="product _item.php?id=1">Product 1</a></li>
-				<li><a href="product _item.php?id=2">Product 2</a></li>
-				<li><a href="product _item.php?id=3">Product 3</a></li>
-				<li><a href="product _item.php?id=4">Product 4</a></li>
+				<li><a href="product_item.php?id=1">Product 1</a></li>
+				<li><a href="product_item.php?id=2">Product 2</a></li>
+				<li><a href="product_item.php?id=3">Product 3</a></li>
+				<li><a href="product_item.php?id=4">Product 4</a></li>
 			</ul>
 		</div>
 	</div>

ca9e4eda1ead2732d025b1545de75414606768ae
김주연 <jennykim4019@gmail.com>
2022-10-30 18:58:01 -0700

    update

--- parts/meta.php
+++ parts/meta.php
@@ -1,6 +1,8 @@
 	
 	<meta name="viewport" content="width=device-width">
 
+	<base href="http://jennykim.space/aau/ixd608/kim.jenny/">
+
 	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
 	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" type="text/css" href="css/storetheme.css">

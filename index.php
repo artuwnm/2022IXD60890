d95ecda59875a944584c87d337e243131772056a
김주연 <jennykim4019@gmail.com>
2022-11-05 03:04:01 -0700

    update

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+
+	<?php include "parts/meta.php"; ?>
+
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Home</h2>
+		</div>
+	</div>
+
+	<div class="container">
+        <article id="article1" class="article card soft">
+            <div class="display-flex flex-align-center">
+                <div class="flex-stretch">
+                    <h2>Article 1</h2>
+                </div>
+                <div class="flex-none">
+                    <small>5:20am</small>
+                </div>
+            </div>
+            <div class="article-body">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus cum autem recusandae asperiores incidunt soluta nisi, at vel minima illum aspernatur quis culpa ullam facilis deleniti esse officiis eos impedit consequuntur. Soluta, iure dolor rem. Cum similique, exercitationem asperiores numquam.</p>
+                <p>Quibusdam fugit modi, eum doloremque, esse vel doloribus, rerum voluptas consectetur, odio magnam tempore consequuntur maxime recusandae? Explicabo harum numquam aliquid blanditiis assumenda saepe sit ex. Cum officiis architecto, similique magnam vel esse odit, id autem, maxime rerum nobis facilis?</p>
+                <p>Nostrum at tempore beatae quam neque reiciendis ab necessitatibus sit, voluptas explicabo nam eaque. Aliquid fugit perferendis aperiam doloribus voluptatem eius soluta sit illo in incidunt, praesentium, architecto quo ratione deleniti molestiae voluptate dolores rerum ex sequi iusto id, asperiores!</p>
+            </div>
+        </article>
+    </div>
+
+
+</body>
+</html>
--- product_list.php
+++ product_list.php
@@ -29,6 +29,16 @@
 
 					<div class="col-xs-12 col-md-4">
 						<figure class="figure product">
+							<img src="img/brownbowl.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=1">Brown Bowl</a></div>
+								<div>$3.99</div>
+							</figcaption>
+						</figure>
+					</div>
+
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
 							<img src="img/darkgreen.jpeg">
 							<figcaption>
 								<div><a href="product_item.php?id=2">Dark Green Cup Set</a></div>
@@ -42,6 +52,83 @@
 							<img src="img/heartcup Large.jpeg">
 							<figcaption>
 								<div><a href="product_item.php?id=3">Heart Cup Set</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/bubblecup.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Bubble Cup</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/flowerplate.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Pink Blue Flower Plate</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/pinkflowerset.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Pink Flower Set</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/puppyplate.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Puppy Plate</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/rabbitplate.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Rabbit Plate</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/smilebowl.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Snoopy Smile Bowl</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/snoopyset.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Snoopy Plate Set</a></div>
+								<div>$8.99</div>
+							</figcaption>
+						</figure>
+					</div>
+
+					<div class="col-xs-12 col-md-4">
+						<figure class="figure product">
+							<img src="img/whitebowlset.jpeg">
+							<figcaption>
+								<div><a href="product_item.php?id=3">Modern White Ceramic Set</a></div>
 								<div>$8.99</div>
 							</figcaption>
 						</figure>

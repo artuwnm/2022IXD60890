b612154a5426b423fda10433661546869deecaf1
김주연 <jennykim4019@gmail.com>
2022-11-12 14:38:41 -0800

    update

--- product_list.php
+++ product_list.php
@@ -12,127 +12,28 @@
 	<?php include "parts/navbar.php"; ?>
 
 	<div class="container">
-		<div class="card soft">
-
 			<h3>Product List</h3>
-
 				<div class="grid gap">
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/bowl.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=1">Bowl Set</a></div>
-								<div>$3.99</div>
-							</figcaption>
-						</figure>
-					</div>
+				
+				<?php
+				include_once "lib/php/functions.php";
+				include_once "parts/templates.php";
 
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/brownbowl.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=1">Brown Bowl</a></div>
-								<div>$3.99</div>
-							</figcaption>
-						</figure>
-					</div>
+				$result = makeQuery(
+						makeConn(),
+						"
+						SELECT *
+						FROM `products`
+						ORDER BY `date_create` DESC
+						LIMIT 12
+						"
 
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/darkgreen.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=2">Dark Green Cup Set</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
+					);
 
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/heartcup Large.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Heart Cup Set</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
+					echo "<div class = 'productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
 
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/bubblecup.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Bubble Cup</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/flowerplate.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Pink Blue Flower Plate</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
+				?>
 
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/pinkflowerset.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Pink Flower Set</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/puppyplate.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Puppy Plate</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/rabbitplate.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Rabbit Plate</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
-
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/smilebowl.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Snoopy Smile Bowl</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
-
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/snoopyset.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Snoopy Plate Set</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
-
-					<div class="col-xs-12 col-md-4">
-						<figure class="figure product">
-							<img src="img/whitebowlset.jpeg">
-							<figcaption>
-								<div><a href="product_item.php?id=3">Modern White Ceramic Set</a></div>
-								<div>$8.99</div>
-							</figcaption>
-						</figure>
-					</div>
 		
 		</div>
 			

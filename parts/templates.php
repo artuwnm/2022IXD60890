9cf373f7648e22542361ed46a03020c44f081978
김주연 <jennykim4019@gmail.com>
2022-11-13 11:39:32 -0800

    update

--- parts/templates.php
+++ parts/templates.php
@@ -1,38 +1,37 @@
 <?php
 
 
-function productListTemplate($r,$o){
-return $r.<<<HTML 
+function productListTemplate($r,$o) {
+return $r.<<<HTML
 <a class="col-xs-12 col-md-4" href="product_item.php?id=$o->id">
-	<figure class ="figure product display-flex flex-column">
-		<div class="flex-stretch">
-			<img src="img/$o->thumnail">
-		</div>
-			<figcaption class="flex-none">
-				<div>&dollar;$o->price</div>
-				<div>$o->title</div>
-			</figcaption>
-	</figure>
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+            <img src="./img/$o->thumbnail"; alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>&dollar;$o->price</div>
+			<div>$o->name</div>
+        </figcaption>
+    </figure>
 </a>
-
 HTML;
-
 }
 
 
 function cartListTemplate($r,$o){
 return $r.<<<HTML
 <div class="display-flex">
-	<div class="flex-none images-thumbs">
-		<img src="img/$o->thumbnail">
-	</div>
-	<div class="flex-stretch">
-		<strong>$o->title</strong>
-		<div>Delete</div>
-	</div>
-	<div class="flex-none">
-		&dollar;$o->price
-	</div>
-</div>
-HTML;
-}
+    <div class="flex-none images-thumbs">
+        <img src="./img/$o->thumbnail">
+    </div>
+    <div class="flex-stretch">
+        <strong>$o->name</strong>
+        <div>Delete</div>
+        </div>
+        <div class="flex-none">
+        &dollar;$o->price
+    </div>
+    </div>
+HTML
+
+;}
--- product_list.php
+++ product_list.php
@@ -12,32 +12,32 @@
 	<?php include "parts/navbar.php"; ?>
 
 	<div class="container">
-			<h3>Product List</h3>
-				<div class="grid gap">
-				
-				<?php
-				include_once "lib/php/functions.php";
-				include_once "parts/templates.php";
-
-				$result = makeQuery(
-						makeConn(),
-						"
-						SELECT *
-						FROM `products`
-						ORDER BY `date_create` DESC
-						LIMIT 12
-						"
-
-					);
-
-					echo "<div class = 'productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
-
-				?>
-
 		
+			<h2>Product List</h2>
+
+
+			<?php
+
+			include_once "lib/php/functions.php";
+			include_once "parts/templates.php";
+
+			$result = makeQuery(
+				makeConn(),
+				"
+				SELECT *
+				FROM `products` 
+				ORDER BY `date_create` DESC
+				LIMIT 12
+				"
+			);
+
+			echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
+
+			?>
+
 		</div>
-			
 	</div>
 
+	
 </body>
 </html>

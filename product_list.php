2c0937645d4f1c88efac85f11f0e2be594ff0d6e
김주연 <jennykim4019@gmail.com>
2022-11-27 19:25:11 -0800

    12 update

--- product_list.php
+++ product_list.php
@@ -10,6 +10,10 @@ include_once "parts/templates.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Product List</title>
 	<?php include "parts/meta.php"; ?>
+
+	<script src="lib/js/functions.js"></script>
+	<script src="js/templates.js"></script>
+	<script src="js/product_list.js"></script>
 </head>
 <body>
 
@@ -17,22 +21,45 @@ include_once "parts/templates.php";
 	
 	<div class="container">
 		<h2>Product List</h2>
+
+		<div class="form-control">
+			<form class="hotdog light" id="product-search">
+				<input type="search" placeholder="Search Products">
+			</form>
+		</div>
       
-		<?php
+		<div class="form-control ">
+			<div class="card soft">
+				<div class="display-flex">
+					<div class="flex-stretch display-flex">
+						<div class="flex-none category-button">
+							<button data-filter="category" data-value="" type="button" class="form-button">All</button>
+						</div>
+						<div class="flex-none category-button">
+							<button data-filter="category" data-value="bowl" type="button" class="form-button">Bowl</button>
+						</div>
+						<div class="flex-none category-button">
+							<button data-filter="category" data-value="plate" type="button" class="form-button">Plate</button>
+						</div>
+						<div class="flex-none category-button">
+							<button data-filter="category" data-value="cup" type="button" class="form-button">Cup</button>
+						</div>
+					</div>
+					<div class="flex-none">
+						<div class="form-select">
+							<select class="js-sort">
+								<option value="1">Newest</option>
+								<option value="2">Oldest</option>
+								<option value="3">Least Expensive</option>
+								<option value="4">Most Expensive</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-		$result = makeQuery(
-			makeConn(),
-			"
-			SELECT *
-			FROM `products`
-			ORDER BY `date_create` DESC
-			LIMIT 12
-			"
-		);
-
-		echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
-
-		?>
+		<div class='productlist grid gap'></div>
 	</div>
 
 </body>

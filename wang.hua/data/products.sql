e273868d1e01ca6cd305310fb75e747c830a7057
wanghua0309 <wanghua0309@gmail.com>
2022-12-08 14:43:06 -0800

    module 14 final

--- wang.hua/data/products.sql
+++ wang.hua/data/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 01, 2022 at 04:52 PM
+-- Generation Time: Dec 08, 2022 at 03:41 PM
 -- Server version: 5.7.39-cll-lve
 -- PHP Version: 7.4.30
 
@@ -54,10 +54,10 @@ INSERT INTO `products` (`id`, `title`, `category`, `description`, `price`, `date
 (7, 'Friends in Bowls', 'under 3', 'These happy little wooden figures, made from different types of wood from trees indigenous to central Europe, are gender neutral. With no specific facial expression, they can be either sad or happy.', '58.00', '2022-11-04 20:33:14', '2022-11-04 20:33:14', 'friends.jpg', 'friends.jpg,friends_1.jpeg,friends_2.jpeg', '99'),
 (8, 'Color Spiral', '3 years old', 'Imaginative young builders can make two dimensional graphic patterns and images, three dimensional towers, houses, fences, ships and so much more with this set.', '145.00', '2022-11-04 20:35:11', '2022-11-04 20:35:11', 'color_spiral.jpg', 'color_spiral.jpg,color_spiral_1.jpeg,color_spiral_2.jpeg', '99'),
 (9, 'Counterrotating Stepped Spiral', '3 years old', 'The Counterrotating Stepped Spiral is fantastic to use in building up mysterious spiral staircases in a palace or to create a tower with a steep staircase or to make an imaginary place accessible.', '190.00', '2022-11-04 20:37:03', '2022-11-04 20:37:03', 'spiral.jpg', 'spiral.jpg,stepped_spiral_1.jpeg,stepped_spiral_2.jpeg', '99'),
-(10, 'Rainbow Rolling Wheel', '3 years old', 'This robust colourful wheel inspires children to crawl and walk after it: wooden beads and a bell inside makes sound and the colours gradiate like the rainbow colours while rolling.', '46.00', '2022-11-04 20:39:31', '2022-11-04 20:39:31', 'wheel.jpeg', 'wheel.jpeg,wheel_1.jpeg,wheel_2.jpeg', '99'),
+(10, 'Rainbow Rolling Wheel', '3 years old', 'This robust colourful wheel inspires children to crawl and walk after it: wooden beads and a bell inside makes sound and the colours gradiate like the rainbow colours while rolling.', '46.00', '2022-11-04 20:39:31', '2022-12-07 20:36:13', 'wheel.jpeg', 'wheel.jpeg,wheel_1.jpeg,wheel_2.jpeg', '99'),
 (11, 'Annual Ring', '3 years old', 'Each segment of the Annual Ring represents 1 month (dark blue represents January, purple represents December). Each week a candle or marble moves one hole further, for instance on Mondays.', '137.00', '2022-11-04 20:43:47', '2022-11-04 20:43:47', 'annual_ring.jpeg', 'annual_ring.jpeg,annual_ring_1.jpeg,annual_ring_2.jpeg', '99'),
-(12, 'Houses', '3 years old', 'For little city planner: Create modern cities and villages with barns and churches. Also as colorful landscape for every wooden train and rail set. Because of the simple shape it is easy to stack and build with the small houses.', '74.00', '2022-11-04 20:53:02', '2022-11-04 20:53:02', 'houses.jpeg', 'houses.jpeg,houses_1.jpeg,houses_2.jpeg', '99'),
-(13, 'Large Rainbow', '3 years old', 'The large Rainbow is really versatile: small children stack, sort and build and as the children get older they will use it as a cradle for dolls, as fence for animals, like a tunnel or bridge for vehicles...', '96.00', '2022-12-01 10:47:25', '2022-12-01 10:47:25', 'rainbow.jpeg', 'rainbow.jpeg,rainbow_1.jpeg,rainbow_2.jpeg', '50');
+(12, 'Houses', '3 years old', 'For little city planner: Create modern cities and villages with barns and churches. Also as colorful landscape for every wooden train and rail set. Because of the simple shape it is easy to stack and build with the small houses.', '74.00', '2022-11-04 20:53:02', '2022-12-07 20:33:05', 'houses.jpeg', 'houses.jpeg,houses_1.jpeg,houses_2.jpeg', '100'),
+(18, 'Large Rainbow', '3 years old', 'The large Rainbow is really versatile: small children stack, sort and build and as the children get older they will use it as a cradle for dolls, as fence for animals, like a tunnel or bridge for vehicles, as house for dwarfs and dollhouse dolls, build amazing sculptures... this Tunnel will always be integrated in playing with a lot of fantasy.', '96.03', '2022-12-05 17:33:11', '2022-12-07 20:13:02', 'rainbow.jpeg', 'rainbow.jpeg,rainbow_1.jpeg,rainbow_2.jpeg', '44');
 
 --
 -- Indexes for dumped tables
@@ -77,7 +77,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
